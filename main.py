e6de694bba58734fbf9f0fa0cbb6f2f7af00c49e
zawo <alikstepaniuk@gmail.com>
2024-10-18 17:18:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+from old_blackjack import setup_blackjack_commands
+
+# ======================== DISCORD BOT ========================
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+# ======================== DISCORD BOT EVENTS ========================
+
+@bot.event
+async def on_ready():
+
+    # ustaw status bota jako niewidoczny
+    # await bot.change_presence(status=discord.Status.invisible)
+
+    print(f'bot zalogowany jako {bot.user}')
+    try:
+        synced = await bot.tree.sync()
+        print(f'Synced {len(synced)} slash commands.')
+    except Exception as e:
+        print(e)
+
+    # await setup_blackjack_game(bot) # <------------------------------------ THIS NEW
+
+    # await channel.send("bot is online")
+
+# ======================== MAREK ========================
+
+# setup_marek_commands(bot)
+
+# ======================== BLACKJACK ========================
+
+setup_blackjack_commands(bot)
+
+# ======================== RUN DISCORD BOT ========================
+
+bot.run(token)
