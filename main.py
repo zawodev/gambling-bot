bfb1a8b3950dd91dffc96538d1b3a4ad74c1aa6e
zawo <alikstepaniuk@gmail.com>
2024-11-04 00:13:26 +0100

    minor update i forgot

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ async def on_ready():
         print(e)
 
     # send message to channel with id 1250873886426402937
-    #channel = await bot.fetch_channel(1250873886426402937)
-    #await channel.send("bot is online")
+    channel = await bot.fetch_channel(1250873886426402937)
+    await channel.send("bot 2.0 is online")
 
 # ======================== MAREK ========================
 
